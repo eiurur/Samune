4d5d96b664a32c83eb2592b6ef20277e57e34dd1
eiurur <eiurur789@gmail.com>
2021-03-06 12:33:14 +0900

    Update test code

--- test/success.spec.js
+++ test/success.spec.js
@@ -8,7 +8,8 @@ const JPG_URL_LIST = [
   'https://68.media.tumblr.com/8435bb2072923a0e3d05885c6f074a4b/tumblr_op2hxiLMta1qbgq3so1_1280.jpg',
   'https://68.media.tumblr.com/25cb865c25db2a80d4e5479f4eceee38/tumblr_oq3d6fXiPk1qzljvuo1_1280.jpg',
 ];
-const GIF_URL = 'https://68.media.tumblr.com/29ff438ae72824d1927da59ba7715b6a/tumblr_opo8okeNMK1twgfw0o3_540.gif';
+const GIF_URL =
+  'https://68.media.tumblr.com/29ff438ae72824d1927da59ba7715b6a/tumblr_opo8okeNMK1twgfw0o3_540.gif';
 const JPG_PATH = './test/images/syaro.jpg';
 const HEAVY_PNG_PATH = './test/images/6MBover.png';
 const WEBP_PATH = './test/images/webp.webp';
@@ -60,12 +61,12 @@ test('should generate test_w#{size}.jpg when pass filename with the name test', 
   await check(t, params, opts, result);
 });
 
-test('should generate test_w#{size}.jpg when pass filename and imageMagickCustomArgs with the name test', async (t) => {
+test('should generate test_w#{size}.jpg when pass filename and resizeOptions with the name test', async (t) => {
   const params = {
     url: JPG_URL_LIST[1],
     filename: 'test2',
     dstDir: THUMBNAIL_DIR,
-    imageMagickCustomArgs: ['-define', 'jpeg:size=120x30'],
+    resizeOptions: ['-define', 'jpeg:size=120x30'],
   };
   const opts = {
     sizes: [30, 120, 240, 480],
